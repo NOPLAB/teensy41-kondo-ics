2d5e4b14b4818f53872d60fc1a0cfd39fae72185
nop <noplab90@gmail.com>
2024-01-16 21:16:01 +0900

    近藤科学のサーボが動いた！！

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,153 @@
-//! The starter code slowly blinks the LED, sets up
-//! USB logging, and creates a UART driver using pins
-//! 14 and 15. The UART baud rate is [`UART_BAUD`].
+//! Demonstrates a loopback UART peripheral.
 //!
-//! Despite targeting the Teensy 4.0, this starter code
-//! also works on the Teensy 4.1.
+//! It uses the alpha board, with the following pinout:
+//!
+//! - Pin 14 is TX.
+//! - Pin 15 is RX.
+//!
+//! Baud rate is 115200bps.
+//!
+//! Every time you send the Teensy a character, it replies with
+//! that same character, and it toggles the LED.
 
 #![no_std]
 #![no_main]
 
-use bsp::board;
+use core::{
+    convert::Infallible,
+    future::{self, IntoFuture},
+};
+
+use embedded_hal::{
+    blocking::{self, serial::Write},
+    serial::Read,
+};
+use rtic_monotonics::imxrt;
 use teensy4_bsp as bsp;
 use teensy4_panic as _;
 
-use bsp::hal::timer::Blocking;
-
-use core::fmt::Write as _;
-
-/// CHANGE ME to vary the baud rate.
-const UART_BAUD: u32 = 115200;
-/// Milliseconds to delay before toggling the LED
-/// and writing text outputs.
-const DELAY_MS: u32 = 500;
+use bsp::{
+    board,
+    hal::{lpuart::ReadFlags, timer::Blocking},
+    ral,
+};
 
 #[bsp::rt::entry]
 fn main() -> ! {
-    // These are peripheral instances. Let the board configure these for us.
-    // This function can only be called once!
-    let instances = board::instances();
-
-    // Driver resources that are configured by the board. For more information,
-    // see the `board` documentation.
     let board::Resources {
-        // `pins` has objects that represent the physical pins. The object
-        // for pin 13 is `p13`.
+        pit,
         pins,
-        // This is a hardware timer. We'll use it for blocking delays.
-        mut gpt1,
-        // These are low-level USB resources. We'll pass these to a function
-        // that sets up USB logging.
-        usb,
-        // This is the GPIO2 port. We need this to configure the LED as a
-        // GPIO output.
         mut gpio2,
-        // This resource is for the UART we're creating.
-        lpuart2,
+        mut gpio4,
+        lpuart6,
+        usb,
         ..
-    } = board::t40(instances);
-
-    // When this returns, you can use the `log` crate to write text
-    // over USB. Use either `screen` (macOS, Linux) or PuTTY (Windows)
-    // to visualize the messages from this example.
+    } = board::t41(board::instances());
     bsp::LoggingFrontend::default_log().register_usb(usb);
-
-    // This configures the LED as a GPIO output.
+    let mut delay = Blocking::<_, { board::PERCLK_FREQUENCY }>::from_pit(pit.0);
     let led = board::led(&mut gpio2, pins.p13);
+    let mut enable_pin = gpio4.output(pins.p2);
+    let mut lpuart6: board::Lpuart6 = board::lpuart(lpuart6, pins.p1, pins.p0, 115200);
+    lpuart6.disable(|lpuart| {
+        // lpuart.enable_fifo(Watermark::tx(3));
+        // lpuart.enable_fifo(Watermark::rx(NonZeroU32::new(3).unwrap()));
+        lpuart.set_parity(Some(bsp::hal::lpuart::Parity::Even));
+    });
 
-    // Configures the GPT1 timer to run at GPT1_FREQUENCY. See the
-    // constants below for more information.
-    gpt1.disable();
-    gpt1.set_divider(GPT1_DIVIDER);
-    gpt1.set_clock_source(GPT1_CLOCK_SOURCE);
+    // loop {
+    //     led.toggle();
+    //     let byte = nb::block!(lpuart6.read()).unwrap();
+    // nb::block!(lpuart6.write(byte)).unwrap();
+    // }
 
-    // Convenience for blocking delays.
-    let mut delay = Blocking::<_, GPT1_FREQUENCY>::from_gpt(gpt1);
+    enable_pin.clear();
 
-    // Create the UART driver using pins 14 and 15.
-    // Cast it to a embedded_hal trait object so we can
-    // use it with the write! macro.
-    let mut lpuart2: board::Lpuart2 = board::lpuart(lpuart2, pins.p14, pins.p15, UART_BAUD);
-    let lpuart2: &mut dyn embedded_hal::serial::Write<u8, Error = _> = &mut lpuart2;
+    led.set();
+    delay.block_ms(1000);
+    led.clear();
 
-    let mut counter: u32 = 0;
     loop {
-        led.toggle();
-        log::info!("Hello from the USB logger! The count is {counter}");
-        write!(
-            lpuart2,
-            "Hello from the UART driver! The count is {counter}\r\n"
-        )
-        .ok();
-
-        delay.block_ms(DELAY_MS);
-        counter = counter.wrapping_add(1);
+        servo_pos(&mut delay, &mut lpuart6, &mut enable_pin, 7500);
+        delay.block_ms(3000);
+        servo_pos(&mut delay, &mut lpuart6, &mut enable_pin, 5500);
+        delay.block_ms(3000);
+        servo_pos(&mut delay, &mut lpuart6, &mut enable_pin, 9500);
+        delay.block_ms(3000);
     }
 }
 
-// We're responsible for configuring our timers.
-// This example uses PERCLK_CLK as the GPT1 clock source,
-// and it configures a 1 KHz GPT1 frequency by computing a
-// GPT1 divider.
-use bsp::hal::gpt::ClockSource;
+fn h_byte(byte: u16) -> u8 {
+    (byte >> 7 & 0x7f) as u8
+}
 
-/// The intended GPT1 frequency (Hz).
-const GPT1_FREQUENCY: u32 = 1_000;
-/// Given this clock source...
-const GPT1_CLOCK_SOURCE: ClockSource = ClockSource::HighFrequencyReferenceClock;
-/// ... the root clock is PERCLK_CLK. To configure a GPT1 frequency,
-/// we need a divider of...
-const GPT1_DIVIDER: u32 = board::PERCLK_FREQUENCY / GPT1_FREQUENCY;
+fn l_byte(byte: u16) -> u8 {
+    (byte & 0x7f) as u8
+}
+
+fn servo_pos<T, D, P, E>(delay: &mut D, uart: &mut T, enable_pin: &mut P, pos: u16)
+where
+    T: Write<u8, Error = Infallible> + Read<u8, Error = ReadFlags>,
+    D: embedded_hal::blocking::delay::DelayUs<u32>,
+    P: embedded_hal::digital::v2::OutputPin<Error = E>,
+    E: core::fmt::Debug,
+{
+    enable_pin.set_high().unwrap();
+
+    delay.delay_us(1000);
+
+    let array = [0x80, h_byte(pos), l_byte(pos)];
+
+    uart.bwrite_all(&array).unwrap();
+
+    delay.delay_us(1000);
+
+    enable_pin.set_low().unwrap();
+
+    for _ in 0..3 {
+        match uart.read() {
+            Ok(byte) => {
+                log::info!("{byte}");
+            }
+            Err(_) => {}
+        };
+    }
+}
+
+fn servo_pos_dma<D, U, const N: u8, P, E>(
+    delay: &mut D,
+    uart: &mut bsp::hal::lpuart::Lpuart<U, N>,
+    enable_pin: &mut P,
+    pos: u16,
+) -> nb::Result<(), Infallible>
+where
+    D: embedded_hal::blocking::delay::DelayUs<u32>,
+    P: embedded_hal::digital::v2::OutputPin<Error = E>,
+    E: core::fmt::Debug,
+{
+    enable_pin.set_high().unwrap();
+
+    delay.delay_us(1000);
+
+    let array = [0x80, h_byte(pos), l_byte(pos)];
+
+    let mut channels = bsp::hal::dma::channels(unsafe { ral::dma::DMA::instance() }, unsafe {
+        ral::dmamux::DMAMUX::instance()
+    });
+
+    uart.dma_write(&mut channels[13].take().unwrap(), &array);
+
+    delay.delay_us(1000);
+
+    enable_pin.set_low().unwrap();
+
+    for _ in 0..3 {
+        match uart.read() {
+            Ok(byte) => {
+                log::info!("{byte}");
+            }
+            Err(_) => {}
+        };
+    }
+
+    Ok(())
+}
